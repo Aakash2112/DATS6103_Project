1c7272c78f445e607490502335eb71e666a3ffd1
Aakash2112 <47492481+Aakash2112@users.noreply.github.com>
2024-12-11 00:42:48 -0500

    Addition New File

---
+++ Project_Main.py
@@ -0,0 +1,12 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+# %%
+# Load the data
+accs = pd.read_csv("accidents_final_data.csv")
+# %%
+accs.head()
+# %%
+
